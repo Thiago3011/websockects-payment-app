c4f3533eb4bdc1ffee62f24dcaa43e2c07a6e327
Thiago3011 <thiago.silva1001@outlook.com>
2025-04-23 19:28:09 -0300

    creating the pix payment

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask, jsonify, request
 from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
-import os
+from payments.pix import Pix
 
 app = Flask(__name__)
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -21,6 +22,11 @@ def create_payment_pix():
     expiration_date = datetime.now() + timedelta(minutes=30)
 
     new_payment = Payment(value=data['value'], expiration_date=expiration_date)
+
+    pix_obj = Pix()
+    data_payment_pix = pix_obj.create_payment()
+    new_payment.bank_payment_id = data_payment_pix["bank_payment_id"]
+    new_payment.qr_code = data_payment_pix["qr_code_path"]
 
     db.session.add(new_payment)
     db.session.commit()
---
+++ payments/pix.py
@@ -0,0 +1,18 @@
+import uuid
+import qrcode
+
+class Pix:
+    def __init__(self):
+        pass
+    
+    def create_payment(self):
+        bank_payment_id = str(uuid.uuid4())
+
+        #copia e cola do pix
+        hash_payment = f'hash_payment_{bank_payment_id}'
+
+        #qrcode
+        img = qrcode.make(hash_payment)
+        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+        
+        return {"bank_payment_id": bank_payment_id, "qr_code_path": f"qr_code_payment_{bank_payment_id}"}
